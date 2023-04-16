9cff503f89188df20950d63e996d5c868115a2b2
Chukwuka Ebuka <chukwuka932@gmail.com>
2023-04-16 23:53:42 +0100

    first commit

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0h%$e+ze2sn!u14b5#7p917v@=$y!&(go6tm*%e18t79_mliu5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
